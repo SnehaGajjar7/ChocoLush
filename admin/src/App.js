c85cb630dedfb51e135c96bc8eb0ba3f58f4c0e8
SnehaGajjar7 <159872816+SnehaGajjar7@users.noreply.github.com>
2025-06-16 11:12:40 +0530

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./component/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Order/Order";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./component/Navbar/Navbar";
 
-function App() {
+const App = () => {
+  const url = "http://localhost:2000";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToastContainer />
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+           <Route path="/orders" element={<Orders url={url} />} /> 
+        </Routes>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
