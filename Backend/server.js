5f27677849e9858e482a0da1186f57c215084d5b
SnehaGajjar7 <159872816+SnehaGajjar7@users.noreply.github.com>
2025-06-18 11:13:11 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,10 @@ import { connectDB } from "./config/db.js";
 import "dotenv/config";
 import userRouter from "./router/userRoute.js";
 import cakeRouter from "./router/CakeRouter.js";
-
+import cartRouter from "./router/cartRouter.js";
+import wishListRouter from "./router/wishListRouter.js";
+import orderRouter from "./router/orderRouter.js";
+import contactRouter from "./router/contactRouter.js";
 
 const app = express();
 const port = 2000;
@@ -17,10 +20,10 @@ connectDB();
 app.use("/api/user", userRouter);
 app.use("/uploads", express.static("uploads"));
 app.use("/api/cake",cakeRouter);
-// app.use("/api/cart",cartRouter)
-// app.use("/api/wishlist", wishListRouter);
-// app.use("/api/order",orderRouter)
-// app.use("/api/contact",contactRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/wishlist", wishListRouter);
+app.use("/api/order",orderRouter)
+app.use("/api/contact",contactRouter)
 
 
 app.listen(port, () =>
